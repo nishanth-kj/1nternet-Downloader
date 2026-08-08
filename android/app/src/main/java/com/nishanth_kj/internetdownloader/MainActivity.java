7b0ab6e245198c8a80ee7771b1b54c50495143c0
nishanth-kj <nishanthkj12@gmail.com>
2026-08-08 19:12:17 +0530

    Initialize C++ (NDK) in Android app

--- android/app/src/main/java/com/nishanth_kj/internetdownloader/MainActivity.java
+++ android/app/src/main/java/com/nishanth_kj/internetdownloader/MainActivity.java
@@ -2,12 +2,27 @@ package com.nishanth_kj.internetdownloader;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'internetdownloader' library on application startup.
+    static {
+        System.loadLibrary("internetdownloader");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // Test native method call
+        Log.d("MainActivity", "Native string: " + stringFromJNI());
     }
+
+    /**
+     * A native method that is implemented by the 'internetdownloader' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
 }
